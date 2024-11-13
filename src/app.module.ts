9b7e508d3d96740765fbf29b35c61846c315e1ce
한바울 <wtg.paulhan@gmail.com>
2024-11-13 14:09:07 +0900

    Feature: configService와 Joi Validator 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configModuleOptions } from './common/config/module.option';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(configModuleOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
