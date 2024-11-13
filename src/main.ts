fe9fabef0b3a02d1e9d2ba45ceae8d1565d5c326
한바울 <wtg.paulhan@gmail.com>
2024-11-13 15:00:32 +0900

    Feature: Swagger 도입

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('woori-practice')
+    .setDescription('우리들의 실력 향상을위한 API 명세서 입니다.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
