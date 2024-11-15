69f85ca80633a0ef7260dad880bbdd7414cb6a8d
한바울 <wtg.paulhan@gmail.com>
2024-11-15 14:17:53 +0900

    Feature: AuthGuard, RoleGuard 추가

--- src/common/guard/auth-guard.ts
+++ src/common/guard/auth-guard.ts
@@ -1,29 +1,61 @@
-import { CanActivate, ExecutionContext, Injectable, Logger, NotFoundException, UnauthorizedException } from "@nestjs/common";
-import { Observable } from "rxjs";
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NotFoundException,
+  NotImplementedException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { ERROR_MESSAGE, TOKEN_SCHEMA } from '../constants/common-constants';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    private static readonly logger = new Logger(AuthGuard.name);
+  private static readonly logger = new Logger(AuthGuard.name);
 
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        try {
-            AuthGuard.logger.log('# AuthGuard 시작');
-            const request = context.switchToHttp().getRequest();
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-            const authorization = request.headers.authorization ?? undefined;
-            AuthGuard.logger.log(`# Authorization: ${authorization}`);
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      AuthGuard.logger.log('AuthGuard 시작');
+      const request = context.switchToHttp().getRequest();
 
-            if(!authorization) {
-                throw new UnauthorizedException();
-            }
+      const authorization = request.headers.authorization || null;
 
-            const [schema, accessToken] = authorization.split(" ");
-            return true;
-        } catch (e) {
-            if(e.message === '존재하지 않는 사용자입니다.') {
-                throw new NotFoundException(e.message);
-            }
-            throw e;
-        }
+      if (!authorization) {
+        AuthGuard.logger.error(`authorization: ${authorization}`);
+        throw new BadRequestException(ERROR_MESSAGE.E002);
+      }
+
+      const [schema, accessToken] = authorization.split(' ');
+
+      if (schema.toLowerCase() !== TOKEN_SCHEMA) {
+        AuthGuard.logger.error(`schema: ${schema}`);
+        throw new BadRequestException(ERROR_MESSAGE.E002);
+      }
+
+      const decoded = this.jwtService.verify(accessToken);
+      AuthGuard.logger.debug(`decoded: ${decoded}`);
+      const user = await this.userService.findOne(decoded.id);
+
+      if (!user) {
+        AuthGuard.logger.error(`user: ${user}`);
+        throw new BadRequestException(ERROR_MESSAGE.E002);
+      }
+
+      request.user = user;
+
+      AuthGuard.logger.log(`AuthGuard 종료`)
+      return true;
+    } catch (e) {
+      AuthGuard.logger.error(`error catch: ${e}`);
+      throw e;
     }
+  }
 }
