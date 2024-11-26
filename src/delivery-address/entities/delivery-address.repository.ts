264cc6a32b6339e061161ea04a413661dda12fd0
한바울 <wtg.paulhan@gmail.com>
2024-11-26 15:02:06 +0900

    Feature: 배송지 등록 추가

--- src/delivery-address/entities/delivery-address.repository.ts
+++ src/delivery-address/entities/delivery-address.repository.ts
@@ -1,8 +1,9 @@
 import { CustomRepository } from "src/common/typeorm/custom.decorator";
 import { Repository } from "typeorm";
 import { DeliveryAddress } from "./delivery-address.entity";
+import { IDeliveryAddressRepository } from "./delivery-address.interface";
 
 @CustomRepository(DeliveryAddress)
-export class DeliveryAddressRepository extends Repository<DeliveryAddress> {
+export class DeliveryAddressRepository extends Repository<DeliveryAddress> implements IDeliveryAddressRepository {
 
 }
