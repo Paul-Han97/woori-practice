01baf52722a99fb953eb4558469f42e3526f8854
한바울 <wtg.paulhan@gmail.com>
2024-12-10 14:39:06 +0900

    Fix: RoomUser Entity에 CommonEntity 추가

--- src/room-user/entities/room-user.entity.ts
+++ src/room-user/entities/room-user.entity.ts
@@ -1,9 +1,10 @@
+import { CommonEntity } from 'src/common/typeorm/common.entity';
 import { Room } from 'src/room/entities/room.entity';
 import { User } from 'src/user/entities/user.entity';
 import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity()
-export class RoomUser {
+export class RoomUser extends CommonEntity {
   @ManyToOne(() => Room)
   @JoinColumn({ referencedColumnName: 'id' })
   room: Room;
